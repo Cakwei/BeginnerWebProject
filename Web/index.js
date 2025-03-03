521d4acf54487d4446ae70dd2c20ec76e97f06da
Cakwei <charleetan2020@gmail.com>
2025-03-03 22:32:57 +0800

    Part 1

---
+++ Web/index.js
@@ -0,0 +1,10 @@
+function burgerOpen() {
+    document.getElementById("menu").style.width = "250px";
+    document.getElementById("menu").style.height = "100vh";
+
+  }
+  
+  function burgerClose() {
+    document.getElementById("menu").style.width = "0";
+    document.getElementById("menu").style.height = "0";
+  }
