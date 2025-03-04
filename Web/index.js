a7168d89a2507d7e85c31914be42725791b42697
Cakwei <charleetan2020@gmail.com>
2025-03-04 12:37:20 +0800

    Added

--- Web/index.js
+++
@@ -1,9 +0,0 @@
-function burgerOpen() {
-    document.getElementById("menu").style.width = "250px";
-    document.getElementById("hamburger-menu").style.display = "none";
-  }
-  
-  function burgerClose() {
-    document.getElementById("menu").style.width = "0px";
-    document.getElementById("hamburger-menu").style.display = "";
-  }
---
+++ index.js
@@ -0,0 +1,10 @@
+function burgerOpen() {
+    document.getElementById("menu").style.width = "250px";
+  }
+  
+  function burgerClose() {
+    document.getElementById("menu").style.width = "0px"; 
+  }
+  function toggleMode() {
+    document.body.classList.toggle("dark-mode");
+  }
