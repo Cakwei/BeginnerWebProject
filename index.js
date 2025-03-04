79f4c961e22871c4081e0a8056fa348f44b80df9
Cakwei <charleetan2020@gmail.com>
2025-03-04 22:02:35 +0800

    Fixed

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function burgerOpen() {
-    document.getElementById("menu").style.width = "250px";
+    document.getElementById("menu").style.transform = "translateX(0px)";
   }
   
   function burgerClose() {
-    document.getElementById("menu").style.width = "0px"; 
+    document.getElementById("menu").style.transform = "translateX(-250px)"; 
   }
   function toggleMode() {
     document.body.classList.toggle("dark-mode");
