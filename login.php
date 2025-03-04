f5c99f99c37a574c74075cb45bdb0aa23793fbe9
Cakwei <charleetan2020@gmail.com>
2025-03-04 13:40:28 +0800

    New

---
+++ login.php
@@ -0,0 +1,112 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account login</title>
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            color: white;
+        }
+        body {
+            background-color: black;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        #title {
+            position: relative;
+            text-align: center;
+            font-size: 2.5em;
+            margin-bottom: 20px;
+        }
+        #container {
+            position: absolute;
+            top: 10vh;
+        }
+        form {
+            display: flex;
+            flex-direction: column;
+            justify-self: center;
+            width: min(100vw, 350px);
+        }
+        #username, #password {
+            color: white;
+            background-color: transparent;
+            border: none;
+            border-bottom: 1px solid white;
+            margin: 10px 0;
+            padding: 0 0 8px 2px;
+            outline: none;
+        }
+        #submit {
+            margin-top: 20px;
+            border: 1px solid white;
+            background-color: transparent;
+            padding: 6px 15px;
+            cursor: pointer;
+        }
+        #linebreak {
+            margin: 30px 0;
+        }
+        #quick-login {
+            display: grid;
+            grid-template-columns: repeat(autofill, 1fr);
+            grid-template-rows: repeat(autofill, 1fr);
+            gap: 10px;
+        }
+        [id^=login_method] {
+            border: 1px solid white;
+            border-radius: 13px;
+            padding: 6px 15px;
+            text-align: center;
+            cursor: pointer;
+        }
+        #login_method_1 {
+            grid-row: 1 / 2;
+        }
+        #login_method_2 {
+            grid-row: 1 / 2;
+        }
+        #login_method_3 {
+            grid-row: 2 / 3;
+        }
+        #login_method_4 {
+            grid-row: 2 / 3;
+        }
+        #home {
+            position: absolute;
+            top: 15px;
+            right: 0;
+        }
+    </style>
+</head>
+<body>
+    <div id="container">
+        <h1 id="title">
+            LOGIN 
+            <a id="home">
+                <svg xmlns="http://www.w3.org/2000/svg" onclick="window.href.location = 'index.html';" width="25" height="25" fill="currentColor" class="bi bi-arrow-return-left" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5"/>
+                </svg>
+            </a>
+        </h1>
+        <form>
+            <input id="username" placeholder="Enter username" name="username"  required type="text">
+            <input id="password" placeholder="Enter password" name="password"  required type="password">
+            <button id="submit" value="submit" text="submit" type="submit">Login</button>
+        </form>   
+        <hr id="linebreak">
+        <div id="quick-login">
+            <a id="login_method_1">Gmail</a>
+            <a id="login_method_2">Facebook</a>
+            <a id="login_method_3">LinkedIn</a>
+            <a id="login_method_4">Twitter</a>
+        </div>
+       
+    </div>
+</body>
+</html>
